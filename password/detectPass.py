45ab5b63d951ce7d64bd15450aa7f3cc7dc2474a
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-05-10 00:03:00 -0400

    Update detectPass.py

--- password/detectPass.py
+++ password/detectPass.py
@@ -1,28 +1,28 @@
-def detect(password):
-    
-    if len(password) < 8:
-        return False
-  
-    num_mayusculas = sum(1 for char in password if char.isupper())
-    num_minusculas = sum(1 for char in password if char.islower())
-    num_digitos = sum(1 for char in password if char.isdigit())
-    num_simbolos = sum(1 for char in password if not char.isalnum())
-    
-    if num_mayusculas < 1 or num_minusculas < 1 or num_digitos < 1 or num_simbolos < 1:
-        return False
-    
-    return True
+import string
+import secrets
 
-def seraBuena(password):
-    if detect(password):
-        return "good"
-    else:
-        return "bad"
+
+def generatePassword(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(secrets.choice(characters) for _ in range(length))
+    return password
+
 
 def main():
-    password = input("Dime tu contraseña: ")
-    clasificacion = seraBuena(password)
-    print(f"contraseña: {clasificacion}")
+    while True:
+        ask = input('length?: ')
+        if not ask.isdigit():
+            print('  a positive integer')
+            continue
 
-if __name__ == "__main__":
-    main()
+        length = int(ask)
+
+        if length <= 0:
+            print('Password length must be an integer')
+            continue
+
+        password = generatePassword(length)
+        print('Generated password:', password)
+        break
+
+main()

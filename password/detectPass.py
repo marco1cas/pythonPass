4a38e38d282f1642691b2d76a11d3ff561d4c8c0
Marco.S <133777873+marco1cas@users.noreply.github.com>
2024-06-06 23:50:10 -0400

    Update detectPass.py

--- password/detectPass.py
+++ password/detectPass.py
@@ -1,28 +1,36 @@
-import string
-import secrets
+def detect(password):
+    if len(password) < 8:
+        return "La contraseña es demasiado corta"
 
+    num_uppercase = sum(1 for char in password if char.isupper())
+    num_lowercase = sum(1 for char in password if char.islower())
+    num_digits = sum(1 for char in password if char.isdigit())
+    num_symbols = sum(1 for char in password if not char.isalnum())
 
-def generatePassword(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(secrets.choice(characters) for _ in range(length))
-    return password
+    if num_uppercase < 1:
+        return "Faltan letras mayúsculas"
+    if num_lowercase < 1:
+        return "Faltan letras minúsculas"
+    if num_digits < 1:
+        return "Faltan dígitos"
+    if num_symbols < 1:
+        return "Faltan caracteres especiales"
 
+    return "Your password is so good"
 
-def main():
-    while True:
-        ask = input('length?: ')
-        if not ask.isdigit():
-            print('  a positive integer')
-            continue
+def isGood(password):
+    result = detect(password)
+    if result == "Your password is so good":
+        return "muy buena"
+    elif len(password) >= 8 and any(char.isdigit() for char in password) and any(char.isalpha() for char in password):
+        return "medio vulnerable"
+    else:
+        return "vulnerable"
 
-        length = int(ask)
+def askthem():
+    password = input('your password: ')
+    clasificacion = isGood(password)
+    print(f"password: {clasificacion}")
 
-        if length <= 0:
-            print('Password length must be an integer')
-            continue
+askthem()
 
-        password = generatePassword(length)
-        print('Generated password:', password)
-        break
-
-main()
